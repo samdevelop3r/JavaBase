746639eb9fa62c9430597c4abd8b643b8a3485dd
Sam <130869088+samdevelop3r@users.noreply.github.com>
2023-07-21 21:18:00 +0300

    Update Convert.java

--- javabase/src/main/java/javamysql/config/Convert.java
+++ javabase/src/main/java/javamysql/config/Convert.java
@@ -1,9 +1,19 @@
 package javamysql.config;
 
+import javamysql.data_classes.Column;
+import javamysql.data_classes.SqlData;
 import javamysql.data_classes.Url;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class Convert {
+
     public static Url asUrl(String databaseUrl) {
         String[] splittedUrl = databaseUrl.split("/");
         String[] hostnameAndPort = splittedUrl[2].split(":");
@@ -14,5 +24,25 @@ public class Convert {
 
     public static String asSqlType(Object columnValue, Object columnType) {
         return columnType.equals(Integer.class) ? columnValue.toString() : "'%s'".formatted(columnValue);
+    }
+
+    public static HashMap<String, Column> getColumns(ResultSet resultSet) throws SQLException {
+        HashMap<String, Column> columns = new HashMap<>();
+        while (resultSet.next()) {
+            columns.put(resultSet.getString("Field"), new Column(resultSet.getString("Field"), resultSet.getString("Type").equals("text") ? String.class : Integer.class));
+        }
+        return columns;
+    }
+
+    public static List<SqlData> asSqlData(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+        ArrayList<SqlData> sqlDataList = new java.util.ArrayList<>();
+
+        while (resultSet.next()) {
+            for (int i = 1; i < resultSetMetaData.getColumnCount() + 1; i++) {
+                sqlDataList.add(new SqlData(resultSetMetaData.getColumnName(i), resultSetMetaData.getColumnTypeName(i).equals("TEXT") ? resultSet.getString(i) : resultSet.getInt(i)));
+            }
+        }
+        return sqlDataList;
     }
 }
